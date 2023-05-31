4e54c4ef7c7e5a1875d0075b396f9d2887912901
0xzac <zheidemann@yelp.com>
2023-05-31 16:07:18 -0700

    component metrics updated

--- ic.py
+++ ic.py
@@ -30,15 +30,15 @@ class IC:
         """Grab IC Component metrics from HELPDESK Project"""
         
         with Bar('Getting IC Component Metrics..', max=len(self.components)) as bar:
-            for i in self.components:
+            for component in self.components:
                 issues = self.jira.search_issues(f'project = HELPDESK AND status in (Resolved, Closed) AND assignee = {self.username} AND \
-                                                resolutiondate >= -{offset}d and component in("{i.name}")')
-                if i.name == 'Fedex' and len(issues):
-                    self.ic_component_metrics[i.name] = {'total_issues':len(issues), 'time': 0, 'inbound_len': 0, 'outbound_len': 0, 'cost_avg': 0}
+                                                resolutiondate >= -{offset}d and component in("{component.name}")')
+                if component.name == 'Fedex' and len(issues):
+                    self.ic_component_metrics[component.name] = {'total_issues':len(issues), 'time': 0, 'inbound_len': 0, 'outbound_len': 0, 'cost_avg': 0}
                     for x in issues:
                         if x.fields.customfield_16736 != None:
                             ib_tracking_num = x.fields.customfield_16736
-                            self.ic_component_metrics[i.name]['inbound_len']+= 1
+                            self.ic_component_metrics[component.name]['inbound_len']+= 1
                             response = self.fedex.track_shipment(ib_tracking_num, return_raw=True)
 
                             try:
@@ -46,25 +46,41 @@ class IC:
                                                                     state=response['output']['completeTrackResults'][0]['trackResults'][0]['recipientInformation']['address']['stateOrProvinceCode'])[0].zip
                                 sender_zip = self.zipDB.find_zip(city=response['output']['completeTrackResults'][0]['trackResults'][0]['shipperInformation']['address']['city'].lower(), \
                                                                 state=response['output']['completeTrackResults'][0]['trackResults'][0]['shipperInformation']['address']['stateOrProvinceCode'])[0].zip
-                            except KeyError:
-                                recipient_zip = None
-                                sender_zip = None
-
-                            cost = float(self.fedex.estimate(sender_zip, recipient_zip, 5, 1000)['totalNetCharge'])
-                            self.ic_component_metrics[i.name]['cost_avg'] += self.ic_component_metrics[i.name]['cost_avg'] 
+                                cost = float(self.fedex.estimate(sender_zip, recipient_zip, 5, 1000, 'FEDEX_2_Day')['totalNetCharge'])
+                            except (KeyError,TypeError):
+                                cost = 0
+    
                         if x.fields.customfield_16737 != None:
                             ob_tracking_num = x.fields.customfield_16737
-                            self.ic_component_metrics[i.name]['outbound_len']+= 1
-                            
+                            self.ic_component_metrics[component.name]['outbound_len']+= 1
+                            response = self.fedex.track_shipment(ob_tracking_num, return_raw=True)
+
+                            try:
+                                recipient_zip = self.zipDB.find_zip(city=response['output']['completeTrackResults'][0]['trackResults'][0]['recipientInformation']['address']['city'].lower(), \
+                                                                    state=response['output']['completeTrackResults'][0]['trackResults'][0]['recipientInformation']['address']['stateOrProvinceCode'])[0].zip
+                                sender_zip = self.zipDB.find_zip(city=response['output']['completeTrackResults'][0]['trackResults'][0]['shipperInformation']['address']['city'].lower(), \
+                                                                state=response['output']['completeTrackResults'][0]['trackResults'][0]['shipperInformation']['address']['stateOrProvinceCode'])[0].zip
+                                cost = float(self.fedex.estimate(sender_zip, recipient_zip, 5, 1000, 'FEDEX_2_DAY')['totalNetCharge'])
+                            except (KeyError,TypeError):
+                                cost = 0
+
+                        date_created = x.fields.created[0:x.fields.created.index('T')]
+                        date_resolved = x.fields.resolutiondate[0:x.fields.resolutiondate.index('T')]
+                        date_delta = datetime.strptime(date_resolved, "%Y-%m-%d").date() - datetime.strptime(date_created, "%Y-%m-%d").date()
+                        self.ic_component_metrics[component.name]['time'] += date_delta.days
+                        self.ic_component_metrics[component.name]['cost_avg'] += cost
+
+                    self.ic_component_metrics[component.name]['time'] /= len(issues)
+                    self.ic_component_metrics[component.name]['cost_avg'] /= self.ic_component_metrics[component.name]['total_issues']
                 elif len(issues):
-                    self.ic_component_metrics[i.name] = {'total_issues':len(issues), 'time': 0}
+                    self.ic_component_metrics[component.name] = {'total_issues':len(issues), 'time': 0}
                     for x in issues:
                         date_created = x.fields.created[0:x.fields.created.index('T')]
                         date_resolved = x.fields.resolutiondate[0:x.fields.resolutiondate.index('T')]
                         date_delta = datetime.strptime(date_resolved, "%Y-%m-%d").date() - datetime.strptime(date_created, "%Y-%m-%d").date()
-                        self.ic_component_metrics[i.name]['time'] += date_delta.days
+                        self.ic_component_metrics[component.name]['time'] += date_delta.days
 
-                    self.ic_component_metrics[i.name]['time'] /= len(issues)
+                    self.ic_component_metrics[component.name]['time'] /= len(issues)
                 bar.next()
 
 
